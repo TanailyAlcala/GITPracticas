1a3ecbac01c4bfb21b8e8c7e550b5fe8b3224da0
TanailyAb <L21100152@nlaredo.tecnm.mx>
2023-10-02 22:36:55 -0500

    Se modifico documento.

---
+++ Parcial 2/ModuloEcma/Areas.js
@@ -0,0 +1,32 @@
+
+export function areaCuadrado(lado)
+{
+    return lado*lado;
+}
+
+export function areaTriangulo(base, altura)
+{
+    return ((base*altura)/2);
+}
+
+export function multiplicacion(num1, num2)
+{
+    return num1*num2;
+}
+export function IMC(peso, estatura)
+{
+    return (peso/(estatura*estatura));
+}
+
+/*module.exports.areacuadrado=areacuadrado;
+module.exports.areatriangulo=areatriangulo;
+module.exports.multiplicacion=multiplicacion;
+module.exports.IMC=IMC;
+
+//console.log(_dirname);
+//console.log(_filename);
+console.log(module);*/
+  
+
+
+
---
+++ Parcial 2/ModuloEcma/CalculoDeAreas.js
@@ -0,0 +1,21 @@
+import * as areas from "./Areas.js";
+
+let areas=require("./Areas.js");
+
+
+let a=areas.areaCuadrado(4);
+
+console.log(a);
+
+
+let t=areas.areatriangulo(4,8);
+console.log(t);
+
+
+let m=areas.multiplicacion(4,8);
+console.log(m);
+
+
+let indicemc=areas.IMC(40,1.52);
+
+console.log(indicemc);
