e8635d2cde330c423b8833e774a874c893359c43
TanailyAb <L21100152@nlaredo.tecnm.mx>
2023-12-06 09:35:14 -0600

    Casi terminado.

--- Parcial 3/ServerExpress/index.js
+++ Parcial 3/ServerExpress/index.js
@@ -7,7 +7,7 @@ const { default: jsPDF } = require('jspdf');
 const path = require('path');
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 const multer = require('multer');
 const upload = multer();
@@ -102,38 +102,7 @@ app.delete('/canciones1', async (req, res) => {
     }
 });
 
-/*app.post('/canciones1', async (req, res) => {
-    const { nombre_cancion, artista, genero, disco, año_lanzamiento } = req.body;
-
-    if (!nombre_cancion || !artista || !genero || !disco || !año_lanzamiento) {
-        return res.status(400).json({ error: "Todos los campos son obligatorios: nombre de la cancion, artista, genero, disco, año de lanzamiento." });
-    }
-
-    if (isNaN(año_lanzamiento)) {
-        return res.status(400).json({ error: "El año de lanzamiento debe ser un número." });
-    }
-
-    try {
-        const connection = await mysql2.createConnection({
-            host: 'localhost',
-            port: '3306',
-            user: 'root',
-            password: 'Aabt.lrna7',
-            database: 'PRACTICA1'
-        });
-
-        const [result] = await connection.execute(
-            `INSERT INTO canciones1 (nombre_cancion, artista, genero, disco, año_lanzamiento) VALUES (?, ?, ?, ?, ?)`,
-            [nombre_cancion, artista, genero, disco, año_lanzamiento]
-        );
-
-        await connection.end();
-        res.json(result);
-    } catch (error) {
-        res.status(500).json({ error: "Error al agregar la canción." });
-    }
-});*/
-app.post('/canciones1', upload.none(), async (req, res) => {
+/*app.post('/canciones1', upload.none(), async (req, res) => {
     const { nombre_cancion, artista, genero, disco, año_lanzamiento } = req.body;
 
     if (!nombre_cancion || !artista || !genero || !disco || !año_lanzamiento) {
@@ -155,6 +124,64 @@ app.post('/canciones1', upload.none(), async (req, res) => {
         res.json(result);
     } catch (error) {
         res.status(500).json({ error: "Error al agregar la canción." });
+    }
+});*/
+/*app.post('/canciones1', (req,res)=>{
+    const nombre_cancion = req.body.nombre_cancion;
+    const artista = req.body.artista;
+    const genero = req.body.genero;
+    const disco = req.body.disco;
+    const año_lanzamiento = req.body.año_lanzamiento;
+
+    const script = `INSERT INTO canciones1 (nombre_cancion, artista, genero, disco, año_lanzamiento) VALUES ('${nombre_cancion}', '${artista}', '${genero}', '${disco}', '${año_lanzamiento}')`;
+
+    connection.query(script, function (err, results, fields){
+        if(results && results.affectedRows == 1){
+            res.status(200).json({ mensaje: "El usuario se ha agregado"});
+        }
+        else{
+            res.status(400).json({mensaje: "No se pudo agregar el usuario"});
+        }
+    })
+});*/
+
+/*app.post('/canciones1', upload.none(), async (req, res) => {
+    const nombre_cancion = req.body.nombre_cancion;
+    const artista = req.body.artista;
+    const genero = req.body.genero;
+    const disco = req.body.disco;
+    const año_lanzamiento = req.body.año_lanzamiento;
+
+    const script = `INSERT INTO canciones1 (nombre_cancion, artista, genero, disco, año_lanzamiento) VALUES ('${nombre_cancion}', '${artista}', '${genero}', '${disco}', '${año_lanzamiento}')`;
+
+    connection.query(script, function (err, results, fields){
+        if(results && results.affectedRows == 1){
+            res.status(200).json({ mensaje: "El usuario se ha agregado"});
+        }
+        else{
+            res.status(400).json({mensaje: "No se pudo agregar el usuario"});
+        }
+    })
+});*/
+app.post('/canciones1', async (req, res) => {
+    const nombre_cancion = req.body.nombre_cancion;
+    const artista = req.body.artista;
+    const genero = req.body.genero;
+    const disco = req.body.disco;
+    const año_lanzamiento = req.body.año_lanzamiento;
+
+    try {
+        const connection = await pool.getConnection();
+        const [result] = await connection.execute(
+            `INSERT INTO canciones1 (nombre_cancion, artista, genero, disco, año_lanzamiento) VALUES (?, ?, ?, ?, ?)`,
+            [nombre_cancion, artista, genero, disco, año_lanzamiento]
+        );
+        connection.release();
+
+        res.status(200).json({ mensaje: "La canción se ha agregado exitosamente" });
+    } catch (error) {
+        console.error("Error al agregar la canción:", error);
+        res.status(500).json({ mensaje: "Error al agregar la canción." });
     }
 });
 
@@ -199,13 +226,13 @@ app.put('/canciones1', async (req, res) => {
 
 app.post('/canciones1/formato', upload.none(), (req, res) => {
     const doc = new jsPDF();
-    doc.text(`Cancion:`, 10, 10);
-    doc.text(`id_cancion: ${req.body.id_cancion}`, 10, 20);
-    doc.text(`nombre_cancion: ${req.body.nombre_cancion}`, 10, 30);
-    doc.text(`artista: ${req.body.artista}`, 10, 40);
-    doc.text(`genero: ${req.body.genero}`, 10, 50);
-    doc.text(`disco: ${req.body.disco}`, 10, 60);
-    doc.text(`año_lanzamiento: ${req.body.año_lanzamiento}`, 10, 70);
+    doc.text(`CANCION`, 10, 10);
+    doc.text(`ID: ${req.body.id_cancion}`, 10, 20);
+    doc.text(`Nombre de la canción: ${req.body.nombre_cancion}`, 10, 30);
+    doc.text(`Artista: ${req.body.artista}`, 10, 40);
+    doc.text(`Genero: ${req.body.genero}`, 10, 50);
+    doc.text(`Disco: ${req.body.disco}`, 10, 60);
+    doc.text(`Año de lanzamiento: ${req.body.año_lanzamiento}`, 10, 70);
     let archivoPDF = path.join(__dirname, 'Archivo.pdf');
     doc.save(archivoPDF);
     res.sendFile(archivoPDF);
